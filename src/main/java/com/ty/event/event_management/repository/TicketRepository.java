9a63ca1bdaf4b2d3ba05ef32f7473e09477ef1c0
vinayjavadeveloper <javadeveloper2@gmail.com>
2022-12-07 12:57:30 +0530

    ticketrepositary done

---
+++ src/main/java/com/ty/event/event_management/repository/TicketRepository.java
@@ -0,0 +1,9 @@
+package com.ty.event.event_management.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ty.event.event_management.dto.Ticket;
+
+public interface TicketRepository extends  JpaRepository<Ticket, Integer> {
+
+}
