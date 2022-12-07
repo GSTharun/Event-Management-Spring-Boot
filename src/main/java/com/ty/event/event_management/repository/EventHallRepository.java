8100eca1f51f3869d5d178f9532aeba473b077fb
GSTharun <gstharun.vs18@gmail.com>
2022-12-07 12:58:07 +0530

    repository added

---
+++ src/main/java/com/ty/event/event_management/repository/EventHallRepository.java
@@ -0,0 +1,9 @@
+package com.ty.event.event_management.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ty.event.event_management.dto.EventHall;
+
+public interface EventHallRepository extends JpaRepository<EventHall, Integer>{
+
+}
