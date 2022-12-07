6692c25664ba985e95ace8dc2e0d08da6716a47c
vinayjavadeveloper <javadeveloper2@gmail.com>
2022-12-07 12:19:53 +0530

    ticket dto done

---
+++ src/main/java/com/ty/event/event_management/dto/Ticket.java
@@ -0,0 +1,38 @@
+package com.ty.event.event_management.dto;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Ticket {
+
+	@Id
+	@GeneratedValue(strategy =  GenerationType.IDENTITY)
+	
+	private int venueid;
+	@NotNull
+	private String venuename;
+	@NotNull
+	private String venueaddress;
+	@NotNull
+	private long phone;
+	
+	//private Venue venue;
+	
+	
+	
+	
+	
+	
+	
+}
