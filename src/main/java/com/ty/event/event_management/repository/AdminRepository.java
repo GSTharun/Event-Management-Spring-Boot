38d7f84336481e805185af5c7a826b84c83b1cc4
sagarb2722 <sagarb2722@gmail.com>
2022-12-07 13:48:38 +0530

    added admin,agent,staff,user

---
+++ src/main/java/com/ty/event/event_management/repository/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.ty.event.event_management.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ty.event.event_management.dto.Admin;
+
+public interface AdminRepository extends JpaRepository<Admin, Integer>{
+
+}
