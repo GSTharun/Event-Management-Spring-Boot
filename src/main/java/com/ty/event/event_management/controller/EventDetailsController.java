abfac1b6db32296c8a8af07432593e4ceceb262a
sahanajavadeveloper <sahanajavadeveloper8009@gamil.com>
2022-12-08 14:46:27 +0530

    Event Details Controller is added

--- src/main/java/com/ty/event/event_management/controller/EventDetailsController.java
+++ src/main/java/com/ty/event/event_management/controller/EventDetailsController.java
@@ -1,5 +1,52 @@
 package com.ty.event.event_management.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ty.event.event_management.dto.EventDetails;
+import com.ty.event.event_management.service.EventDetailsService;
+import com.ty.event.event_management.util.ResponseStructure;
+
+
+@RestController
+@RequestMapping("eventdetails")
 public class EventDetailsController {
+
+	@Autowired
+	private EventDetailsService evService;
+
+
+	
+	@PostMapping
+	public ResponseEntity<ResponseStructure<EventDetails>> saveEventDetails(@RequestBody EventDetails eventDetails){
+		return evService.saveEventDetails(eventDetails);
+	}
+
+
+	@PutMapping
+	public ResponseEntity<ResponseStructure<EventDetails>> updateEventDetails(@RequestBody EventDetails eventDetails,@RequestParam int id){
+		return evService.updateEventDetailsById(eventDetails, id);
+	}
+
+
+	@GetMapping
+	public ResponseEntity<ResponseStructure<EventDetails>> getEventDetailsById(@RequestParam int id){
+		return evService.getEventDetailsById(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<ResponseStructure<EventDetails>> deleteEventDetailsById(@PathVariable int id){
+		return evService.deleteEventDetailsById(id);
+	}
+
 
 }
