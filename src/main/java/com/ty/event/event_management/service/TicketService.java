21b8e8e1ab6a1f6ea4b23dc61537d5c35109ccff
vinayjavadeveloper <javadeveloper2@gmail.com>
2022-12-08 10:55:14 +0530

    Adding TicketService layer

---
+++ src/main/java/com/ty/event/event_management/service/TicketService.java
@@ -0,0 +1,33 @@
+package com.ty.event.event_management.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ty.event.event_management.dao.TicketDao;
+
+@Service
+public class TicketService {
+
+	@Autowired
+	private	TicketDao ticketDao;
+	
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
