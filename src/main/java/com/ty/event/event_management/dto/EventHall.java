eb74a701ed0eb0ea3835261970deb740caa44530
GSTharun <gstharun.vs18@gmail.com>
2023-01-06 17:15:29 +0530

    changes made in dto and services

--- src/main/java/com/ty/event/event_management/dto/EventHall.java
+++ src/main/java/com/ty/event/event_management/dto/EventHall.java
@@ -1,14 +1,12 @@
 package com.ty.event.event_management.dto;
 
-import java.util.List;
+
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
 
@@ -30,9 +28,9 @@ public class EventHall {
 	@NotNull
 	private long phone;
 
-	@OneToMany(cascade = CascadeType.ALL)
-
-	List<EventDetails> evDetails;
+//	@OneToMany(cascade = CascadeType.ALL)
+//
+//	List<EventDetails> evDetails;
 
 	@OneToOne(cascade = CascadeType.ALL)
 	private Address address;
